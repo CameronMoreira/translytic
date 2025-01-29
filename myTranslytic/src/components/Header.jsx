a5f97ba35ae8dc1f11e66750bcfcdc6796d5b87d
Cameron Tomé Moreira <ctm313@gmail.com>
2025-01-29 17:02:38 -0500

    add styling to title

--- myTranslytic/src/components/Header.jsx
+++ myTranslytic/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <header className='flex items-center justify-between gap-4 p-4'>
-        <h1>Trans<span className='text-blue-400'>Lytic</span></h1>
+        <h1 className='fond-medium'>Trans<span className='text-blue-400 bold'>Lytic</span></h1>
         <button className='flex itmes-center gap-2'>
         <p>New</p>
         <i className="fa-solid fa-plus"></i>
--- myTranslytic/src/components/Home.jsx
+++ myTranslytic/src/components/Home.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function Home() {
     return (
-        <main className='flex-1 p-4 flex flex-col justify-center'>
-            howdy
+        <main className='flex-1 p-4 flex flex-col gap-3 text-center sm:gap-4 md:gap-5 justify-center'>
+            <h1 className='font-semibold text-5xl sm:text-6xl'>Trans<span className="text-blue-400 bold">Lytic</span></h1>
+            <h3 className='font-medium'>Record <span 
+            className='text-blue-400'>&rarr;</span> Transcribe <span 
+            className='text-blue-400'>&rarr;</span> Translate</h3>
         </main>
     )
 }
