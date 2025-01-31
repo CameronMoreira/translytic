5ebe0900da14ceedc178a394a42bea42c6892357
Cameron Tomé Moreira <ctm313@gmail.com>
2025-01-31 06:49:52 -0500

    Update stop and start recording based on current state

--- myTranslytic/src/components/Home.jsx
+++ myTranslytic/src/components/Home.jsx
@@ -50,8 +50,20 @@ export default function Home(props) {
             const audioBlob = new Blob(audioChunks, {type: mimeType}) // Create a new blob
             setAudioStream(audioBlob) // Set the audio stream
             setAudioChunks([]) // Reset the audio chunks
+            setDuration(0) // Reset the duration
         }
     }
+
+    useEffect(() => {
+        if (recordingStatus === 'inactive') {
+            return
+        }
+        const interval = setInterval(() => {
+            setDuration((curr) => curr + 1) // Increment the duration by 1
+        }, 1000)
+
+        return () => clearInterval(interval)
+    })
 
     return (
         <main className='flex-1 p-4 flex flex-col gap-3 text-center sm:gap-4 md:gap-5 justify-center pb-20'>
@@ -59,9 +71,14 @@ export default function Home(props) {
             <h3 className='font-medium md:text-lg'>Record <span 
             className='text-blue-400'>&rarr;</span> Transcribe <span 
             className='text-blue-400'>&rarr;</span> Translate</h3>
-            <button className='flex specialBtn px-4 py-2 rounded-xl items-center text-base justify-between gap-4 mx-auto w-72 max-w-full my-4'>
-                <p className='text-blue-400'>Record</p>
-                <i className="fa-solid fa-microphone"></i>
+            <button onClick={recordingStatus === 'recording' ? stopRecording : startRecording} className='flex specialBtn px-4 py-2 rounded-xl items-center text-base justify-between gap-4 mx-auto w-72 max-w-full my-4'>
+                <p className='text-blue-400'>{recordingStatus === 'inactive' ? 'Record' : `Stop recording`}</p>
+                <div className='flex items-center gap-2'> 
+                    {duration && (
+                        <p className='text-sm'>{duration}s</p>
+                    )}
+                    <i className={"fa-solid duration-200 fa-microphone " + (recordingStatus === 'recording' ? 'text-rose-300' : "")}></i>
+                </div>
             </button>
             <p className='text-base'>Or <label className='text-blue-400 cursor-pointer hover:text-blue-600 duration-200'>upload 
                 <input onChange={(e) => {
